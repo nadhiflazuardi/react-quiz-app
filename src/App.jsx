99c887b47727f3681fdeb4ca23b5dd77077e9689
Nadhif Lazuardi <berkasnadhif@gmail.com>
2024-07-28 20:32:38 +0700

    Feat: Create layout and styling for question and options display

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import './index.css';
+import "./index.css";
+import Quiz from "./Components/Quiz/Quiz";
 
 const App = () => {
-  return <div className='m-0'>App</div>;
+  return (
+    <div className="m-0 h-screen bg-gradient-to-r from-cyan-500 to-blue-500 flex items-center justify-center">
+      <Quiz />
+    </div>
+  );
 };
 
 export default App;
--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Quiz = () => {
   return (
-    <div>Quiz</div>
-  )
-}
+    <div
+      id="container"
+      className="px-10 py-8 text-[#262626] bg-white m-auto flex flex-col rounded-xl min-w-[480px]"
+    >
+      <h1 className="mb-2 text-2xl font-semibold">Quiz App</h1>
+      <hr className="h-[2px] border-none bg-[#707070]" />
+      <h2 className="my-2 text-lg">What is the best way to center a div?</h2>
+      <ul>
+        <li className="border-2 rounded-md mb-2 px-3 py-2">Flexbox</li>
+        <li className="border-2 rounded-md mb-2 px-3 py-2">Flexbox</li>
+        <li className="border-2 rounded-md mb-2 px-3 py-2">Flexbox</li>
+        <li className="border-2 rounded-md mb-2 px-3 py-2">Flexbox</li>
+      </ul>
+      <button className="bg-blue-400 rounded-md w25 w-1/3 self-center my-2 py-1 font-semibold">Next</button>
+      <div className='text-center text-sm'>1 of 5 questions</div>
+    </div>
+  );
+};
 
-export default Quiz
+export default Quiz;

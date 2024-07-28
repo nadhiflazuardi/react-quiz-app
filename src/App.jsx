608a99724d6d7e0a52c334f76992def6d23c13d1
Nadhif Lazuardi <berkasnadhif@gmail.com>
2024-07-28 18:48:22 +0700

    chore: install and configure Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import './index.css';
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  return <div className='m-0'>App</div>;
+};
 
-export default App
+export default App;
